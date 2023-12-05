e97745b5bf76cb0a16977681098c1e3868c541d9
Chichop <pedria57@gmail.com>
2023-12-05 13:40:35 -0300

    cetvit commit tender

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+export const URL_FRONT = process.env.URL_FRONT || "http://localhost:5173"
+
+export const DB_HOST = process.env.DB_HOST || "localhost"
+export const DB_PORT = process.env.DB_PORT || 5444
+export const DB_DATABASE = process.env.DB_DATABASE || "postgres"
+export const DB_USER = process.env.DB_USER || "postgres"
+export const DB_PASSWORD = process.env.DB_PASSWORD || "mysecretpassword"
+
+export const PORT = process.env.URL_FRONT || 3000
